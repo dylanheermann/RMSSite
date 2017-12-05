cb2a729609577eb322976f60d6790f6afc0e50f4
Dylan Heermann <dylan.heermann@yahoo.com>
2017-12-05 10:45:26 -0500

    initialized

---
+++ BlueBadgeSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueBadgeSolution.Startup))]
+namespace BlueBadgeSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
