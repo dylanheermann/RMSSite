9a003b5b4f20566f643d0579285f2dc5d137e347
Unknown <dylan.heermann@yahoo.com>
2017-12-07 11:52:28 -0500

    Imported Annotations

---
+++ RMS.Data/SongEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Data
+{
+    public class SongEntity
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Song { get; set; }
+        public string Description { get; set; }
+        public string SongName { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
