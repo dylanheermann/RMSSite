fe018052b288a1accdb17956c175b342b22f3831
Unknown <dylan.heermann@yahoo.com>
2017-12-07 13:28:46 -0500

    Added IQuote Interface

---
+++ RMS.Contracts/ISong.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RMS.Models;
+
+namespace RMS.Contracts
+{
+    public interface ISong
+    {
+        bool CreateSong(SongCreateModel model);
+        IEnumerable<SongListModel> GetSongs();
+        SongDetailsModel GetSongById(int songId);
+        bool UpdateSong(SongEditModel model);
+        bool DeleteSong(int songId);
+    }
+}
