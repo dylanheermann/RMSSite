de388cb53a184a06af8ea5a9dcec6236e21fc546
Unknown <dylan.heermann@yahoo.com>
2017-12-07 11:59:33 -0500

    Before Migrations

--- RMS.Data/IdentityModels.cs
+++ RMS.Data/IdentityModels.cs
@@ -1,8 +1,11 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using RMS.Data;
 
 namespace BlueBadgeSolution.Models
 {
@@ -29,5 +32,55 @@ namespace BlueBadgeSolution.Models
         {
             return new ApplicationDbContext();
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public DbSet<SongEntity> Songs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    public class IdentityUserLoginConfiguration
+      : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration
+        : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
+        }
+
+
+
     }
 }
